851f53ae2deda912d2e687030dd91d5084762256
Karthik Kondu <kondukarthik@gmail.com>
2024-08-12 22:19:39 +0530

    added new features

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,38 @@
+import Header from '@/components/shared/Header'
 import React from 'react'
+import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs/server';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({params: {type}}: SearchParamProps) => {
+
+  const transformation = transformationTypes[type];
+  const {userId} = auth();
+
+  if(!userId) redirect('/sign-in');
+
+  // const user = {
+  //   _id: '66b1550dd92ab98d02960921',
+  //   creditBalance: 10
+  // }
+  const user = await getUserById(userId);
+
   return (
-    <div>
-      AddTransformationTypePage
-    </div>
+    <>
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}/>
+
+      <section className='mt-10'>
+        <TransformationForm
+          action="Add" 
+          userId = {user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}/>
+      </section>
+    </>
   )
 }
 
