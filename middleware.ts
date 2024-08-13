fc4afb8939c8817348070871c5141d937085c298
Karthik Kondu <kondukarthik@gmail.com>
2024-08-14 03:31:14 +0530

    enabled debugging in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,21 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
+const debugStatus = true;
+
 // Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware()
+export default clerkMiddleware(
+  (auth, req) => {
+
+    console.log('=========================');
+    console.log('Request Headers');
+    console.log('host:', req.headers.get('host'));
+    console.log('x-forwarded-host', req.headers.get('x-forwarded-host'));
+    console.log('x-forwarded-proto', req.headers.get('x-forwarded-proto'));
+    console.log('x-forwarded-proto', req.headers.get('x-forwarded-port'));
+    console.log('=========================');
+  },
+  { debug: debugStatus },
+);
 
 export const config = {
   matcher: [
